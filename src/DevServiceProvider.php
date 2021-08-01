23281222a09963aba28581a7e9ef072abf477bdc
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-08-01 23:02:00 +0200

    Initial commit

---
+++ src/DevServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brunocfalcao\Dev;
+
+use Illuminate\Support\ServiceProvider;
+
+final class DevServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
