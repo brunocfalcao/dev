29efe6c72a98284b37f238d9f343863e5816e312
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-08-25 01:10:15 +0200

    wip

--- src/DevServiceProvider.php
+++ src/DevServiceProvider.php
@@ -2,16 +2,18 @@
 
 namespace Brunocfalcao\Dev;
 
+use Eduka\Abstracts\EdukaServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
-final class DevServiceProvider extends ServiceProvider
+final class DevServiceProvider extends EdukaServiceProvider
 {
-    public function register()
+    public function boot()
     {
-        //
+        $this->customViewNamespace(__DIR__.'/../resources/views', 'site');
+        config()->set('mail.mailers.postmark.token', env('POSTMARK_TOKEN'));
     }
 
-    public function boot()
+    public function register()
     {
         //
     }
