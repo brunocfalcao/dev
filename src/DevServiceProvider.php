7f44fb77cbe2a140aa2fa8e06566cda6190b8c04
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2022-07-12 02:22:52 +0200

    Update DevServiceProvider.php

--- src/DevServiceProvider.php
+++ src/DevServiceProvider.php
@@ -8,7 +8,6 @@ final class DevServiceProvider extends EdukaServiceProvider
 {
     public function boot()
     {
-        dd('here');
         $this->customViewNamespace(__DIR__.'/../resources/views', 'site');
         config()->set('mail.mailers.postmark.token', env('POSTMARK_TOKEN'));
     }
